d80d4489ca714bec41fed784fbc55e30d1d31c79
bw98 <786016746@qq.com>
2017-12-18 17:37:52 +0800

    update new solution of #1 TwoSum

--- Array/TwoSum.cpp
+++ Array/TwoSum.cpp
@@ -6,11 +6,11 @@
  ************************************************************************/
  
 //Runtime: 106 ms
+//First thought: BF
 class Solution {
     public:
     vector<int> twoSum(vector<int>& nums, int target) {             
         int i,j;
-        int index1 = 0, index2 = 0;
         vector<int> ret {0, 1};
         for (i = 0; i< nums.size(); ++i) {
             ret[0] = i;
@@ -24,6 +24,29 @@ class Solution {
         }                                 
     }                                                                    
 };                                    
+                                           
+//Runtime: 79 ms
+//Because nums[i], nums[j], target are fixed values, we do not need to do additions every time
+class Solution {
+    public:
+    vector<int> twoSum(vector<int>& nums, int target) {             
+        int i,j;
+        vector<int> ret {0, 1};
+        for (i = 0; i< nums.size(); ++i) {
+            ret[0] = i;
+            int tar = target - nums[i];
+            for(j = i+1; j < nums.size(); ++j){
+                if (nums[j] == tar) 
+                {    
+                    ret[1] = j;
+                    return ret;
+                }                                                          
+            }                                                              
+        }                                 
+    }                                                                    
+};                                    
+                                      
+                                      
                                       
                                       
                                       
---
+++ HashTable/TwoSum.cpp
@@ -0,0 +1,33 @@
+/*************************************************************************
+	> File Name: TwoSum.cpp
+	> Author: bw98
+	> Mail: 
+	> Created Time: 2017年12月15日 星期五 20时33分44秒
+ ************************************************************************/
+ 
+//Runtime: 6 ms
+
+#include<unordered_map>
+using std::unordered_map;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {             
+        unordered_map<int,int> um;
+        vector<int> res(2);
+        int i;
+        int n = nums.size();
+        for (i = 0; i < n; ++i) {
+            if (um.find(target - nums[i]) != um.end()) {
+                res[0] = um[target - nums[i]];
+                res[1] = i; 
+                return res;  
+            }
+            else {
+                um[nums[i]] = i;          
+            }            
+        }
+        um.clear();
+    }   
+};
+
